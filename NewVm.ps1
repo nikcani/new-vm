aa1d481db554b3ca2815fecd686540b302532109
Niklas Canisius <niklascanisius@me.com>
2023-03-05 18:34:06 +0000

    initial commit

---
+++ NewVm.ps1
@@ -0,0 +1,38 @@
+Add-Type -AssemblyName System.Windows.Forms
+Import-Module BitsTransfer
+
+# clear
+Clear-Host
+
+# create folder for vm
+$CurrentDirectory = Get-Location
+$VmName = Read-Host -Prompt 'VM Name'
+$VmPath = "$CurrentDirectory\$VmName"
+If(!(test-path -PathType container $VmPath))
+{
+	New-Item -ItemType Directory -Path $VmPath
+}
+
+# slect base disk
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+	InitialDirectory = $CurrentDirectory
+    Filter = 'Virtual Disk (*.vhdx)|*.vhdx'
+}
+$null = $FileBrowser.ShowDialog()
+$BaseVhd = $FileBrowser.FileName
+
+# copy base disk
+$DiskName = 'Disk'
+Start-BitsTransfer -Source "$BaseVhd" -Destination "$VmPath\$DiskName.vhdx" -DisplayName "Base Disk"
+
+# create vm
+New-VM `
+-Name "$VmName" `
+-BootDevice VHD `
+-Generation 2 `
+-MemoryStartupBytes 4096MB `
+-Path "$CurrentDirectory" `
+-SwitchName 'Default Switch'`
+-VHDPath "$VmPath\$DiskName.vhdx"
+
+Set-VMFirmware "Test VM" -EnableSecureBoot On
